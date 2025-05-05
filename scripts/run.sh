75da86ee500adc7290f9bc9eb29b2b2ed7b4855a
Artem Fatkulin <artem42fatkulin@gmail.com>
2025-05-05 15:25:40 +0300

    Fix missing seed parameter

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
 DATASET=$1
 
 # SASRec
-python run.py -m 42,221,451,934,1984 dataset=$DATASET
+python run.py -m seed=42,221,451,934,1984 dataset=$DATASET
 
 # SASRec with content initialization
-python run.py -m 42,221,451,934,1984 dataset=$DATASET use_pretrained_item_embeddings=True
+python run.py -m seed=42,221,451,934,1984 dataset=$DATASET use_pretrained_item_embeddings=True
 
 # SASRec with trainable delta
-python run.py -m 42,221,451,934,1984 dataset=$DATASET use_pretrained_item_embeddings=True train_delta=True
+python run.py -m seed=42,221,451,934,1984 dataset=$DATASET use_pretrained_item_embeddings=True train_delta=True
